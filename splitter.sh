b977e7045eb8ea5a977e08bd03b9b1a86839b1f4
Saul Lozano <lozano.saul@gmail.com>
2019-10-22 20:41:43 -0600

    Update splitter.sh
    added a marking to the identifiers for the next step.

--- splitter.sh
+++ splitter.sh
@@ -9,11 +9,13 @@ infile="$1"
 inc=$2
 ncol=$(awk 'NR==1{print NF}' "$infile")
 
-((inc < ncol)) || { echo -e "\nSplit size >= number of columns per split file\n\n"; exit; }
+((inc < ncol)) || { echo -e "\nSplit size >= number of columns per file\n\n"; exit; }
 
 for((i=0, start=1, end=$inc; i < ncol/inc + 1; i++, start+=inc, end+=inc)); do
   cut -f$start-$end "$infile" > "${infile}.$i"
 done
 
-#take file with all the first columns and find the unique IDs
+#take file with first column and find the unique IDs
 cat "${infile}".0 | sort | uniq > unique_ids.txt
+#mark each unique identifier with "u", a human mark what corresponds to chrom 1,2,3 or "u" (unassigned)
+awk '{print $0", u"}' unique_ids.txt > marked_ids.txt
